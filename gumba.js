1892d1a9140c7f92ba1c3748fb40fd08a40f5d97
LasseHarm <lasseharm@web.de>
2018-02-21 15:54:17 +0100

    Added existing source code
    moved applkicatin to git

---
+++ gumba.js
@@ -0,0 +1,6 @@
+let gumba = (function() {
+  for (var i = 0; i < 500; i++) {
+    $('.cont').prepend( "<img id='first' src='./tumblr_mhoa2a55Qx1rrftcdo1_500.gif'>");
+    console.log('here');
+  }
+}());
